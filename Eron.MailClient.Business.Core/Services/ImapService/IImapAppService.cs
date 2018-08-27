449a49d9a04bf991d00971357a08176c42ef6cd7
Mohammad Rebati <max-designer@live.com>
2018-08-28 00:21:12 +0430

    initialize project

---
+++ Eron.MailClient.Business.Core/Services/ImapService/IImapAppService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eron.mailClient.Application.Core.Enums;
+using Eron.MailClient.Business.Core.Infrastructure;
+using Eron.MailClient.Business.Core.Services.ImapService.Dto;
+using Eron.MailClient.SharedKernel.Modules.ValueObjects;
+
+namespace Eron.MailClient.Business.Core.Services.ImapService
+{
+    public interface IImapAppService: IApplicationService
+    {
+        IPagedListResult<EmailSummeryDto> GetEmailsByFolder(EmailDefaultFolderType folderType);
+    }
+
+    public class ImapAppService : IImapAppService
+    {
+        public IPagedListResult<EmailSummeryDto> GetEmailsByFolder(EmailDefaultFolderType folderType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
