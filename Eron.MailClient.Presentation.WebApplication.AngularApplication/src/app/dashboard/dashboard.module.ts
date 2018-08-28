f226be1a7b36c31476a431c3037419e1443f4cc3
Mohammad Rebati <max-designer@live.com>
2018-08-28 23:03:06 +0430

    added angular application base - initialize

---
+++ Eron.MailClient.Presentation.WebApplication.AngularApplication/src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BaseModule } from '../base/base.module';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    BaseModule
+  ],
+  providers: []
+})
+export class DashboardModule {
+
+}
